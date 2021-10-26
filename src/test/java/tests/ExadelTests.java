697fc44ad96ade592db366199a6fe902e6c4751a
Andrei Kaliasinski <andykolesinski@gmail.com>
2021-10-26 13:06:06 +0300

    Added annotations

--- src/test/java/tests/ExadelTests.java
+++ src/test/java/tests/ExadelTests.java
@@ -1,15 +1,28 @@
 package tests;
 
+import annotations.JiraIssue;
+import annotations.JiraIssues;
+import annotations.Layer;
+import annotations.Microservice;
 import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
 
+@Layer("UI Tests")
+@Owner("andreik")
 @DisplayName("UI tests for https://exadel.com/")
 public class ExadelTests extends BaseTest {
 
     @Test
+    @Microservice("Main")
+    @JiraIssue("HOMEWORK-267")
+    @Tags({@Tag("web"), @Tag("critical")})
     @Description("TestCase #1")
     @DisplayName("Search icon is visible on Home page")
     public void checkSearchIconIsDisplayed() {
@@ -23,6 +36,9 @@ public class ExadelTests extends BaseTest {
     }
 
     @Test
+    @Microservice("Main")
+    @JiraIssue("HOMEWORK-267")
+    @Tags({@Tag("web"), @Tag("smoke")})
     @Description("TestCase #2")
     @DisplayName("User can open Contact us page from Home page and see Contact form")
     public void userCanOpenContactUsPageAndSeeContactForm() {
@@ -38,6 +54,9 @@ public class ExadelTests extends BaseTest {
     }
 
     @Test
+    @Microservice("Main")
+    @JiraIssue("HOMEWORK-267")
+    @Tags({@Tag("web"), @Tag("smoke")})
     @Description("TestCase #3")
     @DisplayName("User can open Careers page from Home page and see Send your CV form")
     public void userCanOpenCareersPageAndSeeCVForm() {
@@ -53,6 +72,9 @@ public class ExadelTests extends BaseTest {
     }
 
     @Test
+    @Microservice("Jobs")
+    @JiraIssue("HOMEWORK-267")
+    @Tags({@Tag("web"), @Tag("critical")})
     @Description("TestCase #4")
     @DisplayName("User can open search specialty in selected location")
     public void userCanSearchSpecialtyInSelectedLocation() {
@@ -78,6 +100,9 @@ public class ExadelTests extends BaseTest {
     }
 
     @Test
+    @Microservice("Jobs")
+    @JiraIssue("HOMEWORK-267")
+    @Tags({@Tag("web"), @Tag("critical")})
     @Description("TestCase #5 - Will fail on last step because of incorrect locator")
     @DisplayName("User can submit CV")
     public void userCanSendCv() {
